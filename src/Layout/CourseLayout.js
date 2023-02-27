539196c6b8b97a1410a61012d8f221e4f0d2925e
engrkashem <kashemaust@gmail.com>
2023-02-27 10:03:00 +0600

    course layout ouylet is set

--- src/Layout/CourseLayout.js
+++ src/Layout/CourseLayout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Courses from '../components/Courses/Courses';
+import { Outlet } from 'react-router-dom';
 import NavBar from '../components/Shared/NavBar/NavBar';
 
 const CourseLayout = () => {
     return (
         <div>
             <NavBar></NavBar>
-            <Courses></Courses>
+            <Outlet></Outlet>
         </div>
     );
 };
