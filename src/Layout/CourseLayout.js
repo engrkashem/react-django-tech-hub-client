63d4fcd679c33ab61688d4f7b6347f51f012ca9b
naimur <68554905+naimur-naiyimu@users.noreply.github.com>
2023-02-27 20:04:09 +0600

    add coourse details modify

--- src/Layout/CourseLayout.js
+++ src/Layout/CourseLayout.js
@@ -4,9 +4,11 @@ import NavBar from '../components/Shared/NavBar/NavBar';
 
 const CourseLayout = () => {
     return (
-        <div>
+        <div className='overflow-y-scroll'>
             <NavBar></NavBar>
-            <Outlet></Outlet>
+            <div className='mt-20 overflow-y-scroll'>
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
