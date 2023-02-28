08c19cb1619d6fa22e59f11b49919618cbaf8e19
engrkashem <kashemaust@gmail.com>
2023-02-28 10:50:20 +0600

    course details single data fetching error fixed

--- src/components/Courses/CourseDetail.js
+++ src/components/Courses/CourseDetail.js
@@ -1,33 +1,20 @@
-import { useParams } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
+import React from 'react';
+// import axios from 'axios';
 
-const CourseDetail = ( ) => {
-    const courseId = useParams()
-    const [course, setCourse] = useState(null);
+const CourseDetail = () => {
+    const course = useLoaderData()
+    console.log(course.course)
 
-    useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/course/course=${courseId}`)
-            .then(response => {
-                setCourse(response.data.course);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, [courseId]);
+    const { title, course_length, instructor, course_fee, description } = course.course;
 
-    if (!course) {
-        return <div>Loading...</div>;
-    }
-
-    const { title, course_length, instructor, course_fee, description } = course;
 
     return (
         <div>
-            <h1>Course details for {courseId}</h1>
+            {/* <h1>Course details for {courseId}</h1> */}
             <h2>Title: {title}</h2>
             <p>Course length: {course_length}</p>
-            <p>Instructor: {instructor}</p>
+            <p>Instructor: {instructor.userName}</p>
             <p>Course fee: {course_fee}</p>
             <p>Description: {description}</p>
         </div>
