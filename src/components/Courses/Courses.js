2d0b13890645b0c6582b12aba4b4e96fcc9c4a4b
engrkashem <kashemaust@gmail.com>
2023-02-28 12:39:47 +0600

    course card design changed and made responsive

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -14,13 +14,18 @@ const Courses = () => {
     }, []);
 
     return (
-        <div className='nim-h-screen w-full grid grid-cols-1 gap-5 ' style={{ height: '100vh' }}>
-            {
-                courses?.map(course => <CourseCard
-                    key={course.id}
-                    course={course}
-                />)
-            }
+        <div className=' flex gap-2 pb-10'>
+            <div className=' hidden lg:block p-4 w-1/3 bg-base-100 border-r border-blue-100'>
+                <p>side</p>
+            </div>
+            <div className='nim-h-screen w-full grid grid-cols-1 gap-5 px-5 lg:px-20 pt-' style={{ height: '100vh' }}>
+                {
+                    courses?.map(course => <CourseCard
+                        key={course.id}
+                        course={course}
+                    />)
+                }
+            </div>
         </div>
     );
 };
