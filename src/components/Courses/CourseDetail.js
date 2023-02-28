8bcc225f0bd795d0122e029f90a48b0285f4ca3d
engrkashem <kashemaust@gmail.com>
2023-02-28 12:52:40 +0600

    course details design done

--- src/components/Courses/CourseDetail.js
+++ src/components/Courses/CourseDetail.js
@@ -8,18 +8,20 @@ const CourseDetail = () => {
     const course = useLoaderData()
     console.log(course.course)
 
-    const { title, course_length, instructor, course_fee, description, video_playlist_url } = course.course;
+    const { title, instructor, description, video_playlist_url } = course.course;
 
 
     return (
-        <div>
+        <div className='w-full lg:w-3/4 px-5 mx-auto mb-20'>
             {/* <h1>Course details for {courseId}</h1> */}
-            <h2>Title: {title}</h2>
-            <p>Course length: {course_length}</p>
-            <p>Instructor: {instructor.userName}</p>
-            <p>Course fee: {course_fee}</p>
-            <p>Description: {description}</p>
-            <ReactPlayer url={video_playlist_url} controls />
+            <div className='lg:flex justify-between my-10 text-2xl'>
+                <h2><span className=' font-semibold'>Course Name:</span> {title}</h2>
+                <p><span className=' font-semibold'>Instructor Name:</span> {instructor.userName}</p>
+            </div>
+            <p className=' text-justify mb-10'><span className=' font-semibold'>Description:</span> {description}</p>
+            <div className='test flex justify-center w-full'>
+                <ReactPlayer url={video_playlist_url} controls />
+            </div>
         </div>
     );
 };
