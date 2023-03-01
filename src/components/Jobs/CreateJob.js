813e4bf5a1e00f7bd67726c8244f975093aa4c4c
Shihab Al Ratul <mr.ratul66@gmail.com>
2023-03-02 00:54:57 +0600

    CreateJob issue fixed

--- src/components/Jobs/CreateJob.js
+++ src/components/Jobs/CreateJob.js
@@ -1,143 +1,135 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { AuthContext } from "../../contexts/AuthProvider";
 
-const CreateJob = () => {
+const CreateJob = () =>
+{
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const { dbUser } = useContext(AuthContext)
-    console.log(dbUser)
+    
 
-
-    const postJob = (data) => {
-        // data.creator = Number(data.creator)
+    const postJob = (data)=>{
+        data.creator = Number(data.creator)
         const url = `http://127.0.0.1:8000/job/`;
         const request = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify(data)
         }
         fetch(url, request).then(res => res.json())
-            .then(data => {
+        .then(data => {
                 console.log(data)
                 window.location.reload(true)
         })
     }
     
     return(
-        <div className="">
-         
+        <div className="lg:p-5">
             <h1 className="font-bold text-3xl text-primary my-5">Post Job</h1>
             <form className="form-control mx-auto w-4/5" onSubmit={handleSubmit(postJob)}>
-                <label className="label mt-2">
-                    <span className="label-text" >Job Title:</span>
-                </label>
-                <input className="input input-bordered input-primary w-full" placeholder="Enter Job Title"
+                    <label className="label mt-2">
+                        <span className="label-text" >Job Title:</span>
+                    </label>
+                    <input className="input input-bordered input-primary w-full" placeholder="Enter Job Title" 
                     {...register("title", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                >
-                </input>
+                    >
+                    </input>
 
-                <label className="label mt-2">
-                    <span className="label-text" >Company Name:</span>
-                </label>
-                <input className="input input-bordered input-primary w-full" placeholder="Enter Company Name:"
+                    <label className="label mt-2">
+                        <span className="label-text" >Company Name:</span>
+                    </label>
+                    <input className="input input-bordered input-primary w-full" placeholder="Enter Company Name:"
                     {...register("company", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                >
-                </input>
+                    >
+                    </input>
 
-                <label className="label mt-2">
-                    <span className="label-text">Company Details:</span>
-                </label>
-                <textarea className="textarea textarea-primary h-32" placeholder="Write your job description here."
+                    <label className="label mt-2">
+                        <span className="label-text">Company Details:</span>
+                    </label>
+                    <textarea className="textarea textarea-primary h-32" placeholder="Write your job description here."
                     {...register("about_company", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                ></textarea>
+                    ></textarea>
 
-                <label className="label mt-2">
-                    <span className="label-text">Job Description:</span>
-                </label>
-                <textarea className="textarea textarea-primary h-64" placeholder="Write your job description here."
+                    <label className="label mt-2">
+                        <span className="label-text">Job Description:</span>
+                    </label>
+                    <textarea className="textarea textarea-primary h-64" placeholder="Write your job description here."
                     {...register("content", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                ></textarea>
+                    ></textarea>
 
-                <label className="label mt-2">
-                    <span className="label-text" >Job Requirements:</span>
-                </label>
-                <input className="input input-bordered input-primary w-full" placeholder="Enter Job Requirements"
+                    <label className="label mt-2">
+                        <span className="label-text" >Job Requirements:</span>
+                    </label>
+                    <input className="input input-bordered input-primary w-full" placeholder="Enter Job Requirements"
                     {...register("skill_requirements", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                >
-                </input>
-
-                <label className="label mt-2">
-                    <span className="label-text" >Job Type:</span>
-                </label>
-                <input className="input input-bordered input-primary w-full" placeholder="Example:(Remote, Hybrid, Onsite)"
+                    >
+                    </input>
+                    
+                    <label className="label mt-2">
+                        <span className="label-text" >Job Type:</span>
+                    </label>
+                    <input className="input input-bordered input-primary w-full" placeholder="Example:(Remote, Hybrid, Onsite)"
                     {...register("job_type", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                >
-                </input>
+                    >
+                    </input>
 
-                <label className="label mt-2">
-                    <span className="label-text" >Job Location:</span>
-                </label>
-                <input className="input input-bordered input-primary w-full" placeholder="Example:(Remote, Hybrid, Onsite)"
+                    <label className="label mt-2">
+                        <span className="label-text" >Job Location:</span>
+                    </label>
+                    <input className="input input-bordered input-primary w-full" placeholder="Example:(Remote, Hybrid, Onsite)"
                     {...register("location", {
                         required: {
                             value: true,
                             message: 'This field is required'
                         }
                     })}
-                >
-                </input>
+                    >
+                    </input>
 
-                <label className="label mt-2 hidden">
-                    <span className="label-text" >Creator Id:</span>
-                </label>
-                <input
-                    type='number'
-                    className="input input-bordered input-primary w-full hidden"
-                    placeholder="Id of the creator."
-                    defaultValue={dbUser.id}
-                    value={dbUser.id}
+                    <label className="label mt-2">
+                        <span className="label-text" >Creator Id:</span>
+                    </label>
+                    <input type='number' className="input input-bordered input-primary w-full" placeholder="Id of the creator."
                     {...register("creator", {
                         required: {
-                            valueAsNumber: true,
-
+                            valueAsNumber:true,
+                            
                             message: 'This field is required'
                         }
                     })}
-                >
-                </input>
+                    >
+                    </input>
 
-                <button type="submit" className="btn btn-primary text-white mt-5">Create Job</button>
-
+                    <button type="submit" className="btn btn-primary text-white mt-5">Create Job</button>
+                
             </form>
         </div>
     )
