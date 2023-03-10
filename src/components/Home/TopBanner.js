aa44d4754114ec93805fa5a9cf942cbd28d45488
engrkashem <kashemaust@gmail.com>
2023-03-10 11:44:53 +0600

    exploremore button text colore changed in top banner

--- src/components/Home/TopBanner.js
+++ src/components/Home/TopBanner.js
@@ -11,7 +11,7 @@ const TopBanner = () => {
                 <div className='mt-10 lg:mt-1'>
                     <h1 className="text-3xl lg:text-5xl font-bold">Welcome to Tech World</h1>
                     <p className="p-6 text-xl">Explore fantastic features and get chance to communicate and seek help to experts. Take guidance in learning technologies.</p>
-                    <Link to={'/dashboard'}><button className="btn btn-primary">Explore More</button></Link>
+                    <Link to={'/dashboard'}><button className="btn btn-primary text-white">Explore More</button></Link>
                 </div>
             </div>
         </div>
